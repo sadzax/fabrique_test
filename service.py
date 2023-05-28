4db7bc1fea7d7054df59819bbf6a287337449054
Marat Ramazanov <sadzax@gmail.com>
2023-05-28 18:39:54 +0300

    Initial

--- service.py
+++ service.py
@@ -1,7 +1,34 @@
+import datetime
 
 #  Создаём класс рассылки
 class Mailing:
-    def __init__(self):
+    def __init__(self, id, date_start, message, filters, date_stop):
         #  Устанавливаем базовые параметры
-        self.id
+        self.id = id
+        self._date_start = None
+        self.date_start = date_start
+        self.message: str = message
+        self.filters = filters
+        self._date_stop = date_stop
+        self.date_stop = date_stop
+
+    @property
+    def date_start(self):
+        return self._date_start
+
+    @property
+    def date_stop(self):
+        return self._date_stop
+
+    @date_start.setter
+    def date_start(self, value):
+        value = datetime.datetime.strptime(value, '%Y/%m/%d %H:%M:%S')
+        self._date_start = value
+
+    @date_stop.setter
+    def date_stop(self, value):
+        value = datetime.datetime.strptime(value, '%Y/%m/%d %H:%M:%S')
+        self._date_stop = value
+
+
 
